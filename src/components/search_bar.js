67acb180017858d6fd961bc99d93b6c131b9f4aa
Eric Carlson <eric.carlson2972@gmail.com>
2018-07-13 16:15:10 -0400

    1_15

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
+class SearchBar extends Component  {
+    render () {
     return <input />;
-};
-
+}
+}
 
 export default SearchBar;
